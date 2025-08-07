4af00a639ab216585ff7571e2642f22d365184f7
devscarycat <devscarycat@icloud.com>
2025-08-07 16:40:43 +0900

    test20

--- index.js
+++ index.js
@@ -661,7 +661,6 @@
       if (IS_MOBILE && this.playing) {
         e.preventDefault();
       }
-
       if (e.target != this.detailsButton) {
         if (!this.crashed && (Runner.keycodes.JUMP[e.keyCode] || e.type == Runner.events.TOUCHSTART)) {
           if (!this.playing) {
@@ -672,7 +671,6 @@
               errorPageController.trackEasterEgg();
             }
           }
-          //  Play sound effect and jump on starting the game for the first time.
           if (!this.tRex.jumping && !this.tRex.ducking) {
             this.playSound(this.soundFx.BUTTON_PRESS);
             this.tRex.startJump(this.currentSpeed);
