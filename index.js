c7653944adeb9117989776d54d5ef66f2b8818b0
devscarycat <devscarycat@icloud.com>
2025-08-07 16:46:28 +0900

    복원

--- index.js
+++ index.js
@@ -661,6 +661,7 @@
       if (IS_MOBILE && this.playing) {
         e.preventDefault();
       }
+
       if (e.target != this.detailsButton) {
         if (!this.crashed && (Runner.keycodes.JUMP[e.keyCode] || e.type == Runner.events.TOUCHSTART)) {
           if (!this.playing) {
@@ -671,6 +672,7 @@
               errorPageController.trackEasterEgg();
             }
           }
+          //  Play sound effect and jump on starting the game for the first time.
           if (!this.tRex.jumping && !this.tRex.ducking) {
             this.playSound(this.soundFx.BUTTON_PRESS);
             this.tRex.startJump(this.currentSpeed);
@@ -768,6 +770,7 @@
       this.tRex.update(100, Trex.status.CRASHED);
 
       // Game over panel.
+
       if (!this.gameOverPanel) {
         this.gameOverPanel = new GameOverPanel(
           this.canvas,
@@ -780,7 +783,7 @@
       }
 
       // Update the high score.
-      const score = this.distanceMeter.getActualDistance(Math.ceil(this.distanceRan));
+      const score = Math.ceil(this.distanceRan / 10) || 1; // 최소 1점 보장
       console.log("Distance ran:", this.distanceRan, "Calculated score:", score);
       if (score > this.highestScore) {
         this.highestScore = score;
